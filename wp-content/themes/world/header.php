37d58f032f7ee07b7e686774d6e8c8e074ea4d9c
yaj190364-maker <yaj190364@gmail.com>
2025-09-25 06:06:44 +0800

    feat:yaj

---
+++ wp-content/themes/world/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Attractive Modern Header & Footer</title>
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/styleBinLayout.css" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/index.css" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/faqs.css" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/contacts.css" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/detail.css" />
+
+</head>
+<body>
+
+<header>
+  <div class="logo">
+    <img src="<?php echo get_template_directory_uri();?>/assets/img/avatar1.png" alt="Logo" />
+  </div>
+
+  <div class="hamburger" id="hamburger" aria-label="Menu" aria-expanded="false" role="button" tabindex="0">
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+  </div>
+  <nav id="nav">
+    <a href="<?php echo home_url('/');?>">Home</a>
+    <a href="<?php echo home_url('/news');?>">News</a>
+    <a href="<?php echo home_url('/faqs');?>">FAQs</a>
+    <a href="<?php echo home_url('/contacts');?>">Contacts</a>
+  </nav>
+</header>
