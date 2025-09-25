ad7e4d961447ceac4df7198916b67cae418b4210
yaj190364-maker <yaj190364@gmail.com>
2025-09-25 17:47:26 +0800

    fix:yaj/style

--- wp-content/themes/world/footer.php
+++ wp-content/themes/world/footer.php
@@ -2,8 +2,8 @@
 <footer>
   <nav id="footer-nav">
     <a href="<?php echo home_url('/');?>">Home</a>
-    <a href="<?php echo home_url('/news');?>">News</a>
-    <a href="<?php echo home_url('/faqs');?>">FAQs</a>
+    <a href="<?php echo home_url('/new');?>">News</a>
+    <a href="<?php echo home_url('/faq');?>">FAQs</a>
     <a href="<?php echo home_url('/contacts');?>">Contacts</a>
   </nav>
   
@@ -11,8 +11,8 @@
     &copy; 2025 ACE Team. Made by 4-2.
   </div>
 </footer>
+<?php wp_footer();?>
 
 <script src="<?php echo get_template_directory_uri();?>/assets/js/common.js"></script>
-
 </body>
 </html>
